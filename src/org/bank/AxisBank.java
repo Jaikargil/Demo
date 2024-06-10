010298cda6d52510e740c6d633286cb4f82f7fe2
Jaikargil <jaikargils@gmail.com>
2024-06-10 09:22:43 +0530

    Shankar add new methods

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -8,6 +8,11 @@ public class AxisBank extends BankInfo{
 	public void deposit2() {
        System.out.println("Deposit: 50000");
 	}
+	
+	public void deposit3() {
+		System.out.println("Shankar");
+
+	}
    public static void main(String[] args) {
 	AxisBank a = new AxisBank();
 	a.saving();
